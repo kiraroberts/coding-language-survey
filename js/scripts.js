81e4df6f58483423f35af9074f03db26c8bf4ef6
Kira <rosehill1788@protonmail.com>
2019-08-16 12:37:10 -0700

    fix radio button names in html

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,29 @@
 
 $(document).ready(function() {
   $("form").submit(function(event) {
-    var inputFrontBack = $("input:radio[name=front-back]:checked").val();
-    var inputColor = $("input:radio[name=color]:checked").val();
-    var inputVacation = $("input:radio[name=vacation]:checked").val();
-    var inputMusic = $("input:radio[name=music]:checked").val();
-    var inputRather= $("input:radio[name=rather]:checked").val();
+    var inputQuestion1 = $("input:radio[name=experience]:checked").val();
+    var inputQuestion2 = $("input:radio[name=support]:checked").val();
+    var inputQuestion3 = $("input:radio[name=topic]:checked").val();
+    var inputQuestion4 = $("input:radio[name=balance]:checked").val();
+    var inputQuestion5 = $("input:radio[name=openSource]:checked").val();
 
 // BACK END
 
-    if (inputFrontBack === "front-end" && inputColor === "red" && inputVacation === "osaka" && inputMusic === "1995" && inputRather === "geology") {
-      $('#ruby').show();
-      $('#python, #c, #no-match').hide();
-    } else if (inputFrontBack === "back-end" && inputColor === "blue" && inputVacation === "amsterdam" && inputMusic === "1990" && inputRather === "reptiles") {
-      $('#python').show();
-      $('#ruby, #c, #no-match').hide();
-    } else if (inputFrontBack === "dont-care" && inputColor === "green" && inputVacation === "seattle" && inputMusic === "2000" && inputRather === "composer") {
-      $('#c').show();
-      $('#ruby, #python, #no-match').hide();
-    } else {
-      $('#no-match').show();
-      $('#ruby, #python, #c').hide();
-    }
+
+
+    // if () {
+    //   $('#ruby').show();
+    //   $('#python, #c, #no-match').hide();
+    // } else if () {
+    //   $('#python').show();
+    //   $('#ruby, #c, #no-match').hide();
+    // } else if () {
+    //   $('#c').show();
+    //   $('#ruby, #python, #no-match').hide();
+    // } else {
+    //   $('#no-match').show();
+    //   $('#ruby, #python, #c').hide();
+    // }
 
   event.preventDefault();
   });
