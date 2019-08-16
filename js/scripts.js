94c1c9d302486d6f540247e017670c5d6bb264bb
Kira <rosehill1788@protonmail.com>
2019-08-16 12:42:34 -0700

    create a function toadd survey score together

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
+// BACK END
 
+var surveyScore = function(input1, input2, input3, input4, input5) {
+  var score = input1 + input2 + input3 + input4 + input5;
+
+
+  if () {
+    $('#ruby').show();
+    $('#python, #c, #no-match').hide();
+  } else if () {
+    $('#python').show();
+    $('#ruby, #c, #no-match').hide();
+  } else if () {
+    $('#c').show();
+    $('#ruby, #python, #no-match').hide();
+  } else {
+    $('#no-match').show();
+    $('#ruby, #python, #c').hide();
+  }
+
+};
 // FRONT END
 
 $(document).ready(function() {
@@ -9,24 +29,9 @@ $(document).ready(function() {
     var inputQuestion4 = $("input:radio[name=balance]:checked").val();
     var inputQuestion5 = $("input:radio[name=openSource]:checked").val();
 
-// BACK END
+    surveyScore(inputQuestion1, inputQuestion2, inputQuestion3, inputQuestion4, inputQuestion5);
 
+    event.preventDefault();
 
-
-    // if () {
-    //   $('#ruby').show();
-    //   $('#python, #c, #no-match').hide();
-    // } else if () {
-    //   $('#python').show();
-    //   $('#ruby, #c, #no-match').hide();
-    // } else if () {
-    //   $('#c').show();
-    //   $('#ruby, #python, #no-match').hide();
-    // } else {
-    //   $('#no-match').show();
-    //   $('#ruby, #python, #c').hide();
-    // }
-
-  event.preventDefault();
   });
 });
